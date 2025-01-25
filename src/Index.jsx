bd732131e5eae31b9f5fe1e05c6e83c2dc803880
erchandraprakash1999 <udit012435@gmail.com>
2025-01-25 10:14:37 +0530

    updated

--- src/Index.jsx
+++ src/Index.jsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 
 export const Index = () => {
     return (
-        <>
-        <h4>Go To Billing Form Page</h4>
-        <Link to={'/form'}>Click Here</Link>
-        </>
+        <div className="text-center " style={{marginTop: '5rem'}}>
+            <h4>Go To Billing Form Page</h4>
+            <div className="mt-5">
+                <Link to={'/form'} className="button">CLICK HERE</Link>
+            </div>  
+        </div>
     )
 }
